1f9e5d18aef45474cb15882264217ed3f70212ef
amenraprod <amenraprod@gmail.com>
2018-08-15 05:38:48 -0400

    removed border

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ placeNameChangedHandler = val => {
     return (
       <View style={styles.container}>
       <TextInput
-      style={{width: 300, borderColor: 'black', borderWidth: 1}}
+      style={{width: 300}}
       placeholder = "An awesome place"
       value={this.state.placeName}
       onChangeText ={this.placeNameChangedHandler} />
