e532abe95941b6b1f52d487c19a65f6c7bc2be76
amenraprod <amenraprod@gmail.com>
2018-08-15 04:53:40 -0400

    imported TextInput component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        
+
       </View>
     );
   }
