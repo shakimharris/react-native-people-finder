79f08075d38dc46cdc6c926c7d42802c3303eeb3
amenraprod <amenraprod@gmail.com>
2018-08-18 16:18:49 -0400

    added props placename

--- src/components/listItem/listItem.js
+++ src/components/listItem/listItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-const listItem = () => (
+const listItem = (props) => (
  <View>
- <Text></Text>
+ <Text>{props.placeName}</Text>
  </View>
 );
 
