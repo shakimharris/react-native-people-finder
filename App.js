e5d73dec8bd5250cf3a9e30b9477413dda6b7036
amenraprod <amenraprod@gmail.com>
2018-08-18 14:55:39 -0400

    added commas to separate properties

--- App.js
+++ App.js
@@ -41,10 +41,10 @@ const styles = StyleSheet.create({
     //flex: 1,
     flexDirection: "row",
     justifyContent: "space-between"
-  }
+  },
   placeInput: {
    width: "70%"
-  }
+ },
   placeButton: {
     width: "30%"
   }
